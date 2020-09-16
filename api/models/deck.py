0a16d12fdd9f7c7fbbe80d905cd3a6e786beb785
rubysattar <rubysattar@gmail.com>
2020-09-16 11:41:11 -0400

    started deck model

--- api/models/deck.py
+++ api/models/deck.py
@@ -2,12 +2,10 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 # Create your models here.
-class Mango(models.Model):
+class Deck(models.Model):
   # define fields
   # https://docs.djangoproject.com/en/3.0/ref/models/fields/
-  name = models.CharField(max_length=100)
-  ripe = models.BooleanField()
-  color = models.CharField(max_length=100)
+  topic = models.CharField(max_length=100)
   owner = models.ForeignKey(
       get_user_model(),
       on_delete=models.CASCADE
@@ -15,13 +13,11 @@ class Mango(models.Model):
 
   def __str__(self):
     # This must return a string
-    return f"The mango named '{self.name}' is {self.color} in color. It is {self.ripe} that it is ripe."
+    return f"The topic of this deck is '{self.topic}'."
 
   def as_dict(self):
-    """Returns dictionary version of Mango models"""
+    """Returns dictionary version of Deck models"""
     return {
         'id': self.id,
-        'name': self.name,
-        'ripe': self.ripe,
-        'color': self.color
+        'topic': self.topic
     }
